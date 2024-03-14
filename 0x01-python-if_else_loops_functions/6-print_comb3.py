48310525ae23d175eda65ae5bcfd94025cb23bca
msDee060 <dananamercy@gmail.com>
2024-03-14 04:52:08 -0700

    is-lower

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for num1 in range(0, 10):
     for num2 in range(num + 1, 0):
         if num1 == 8 and num2 == 9:
             print(("{}{}".format(num1, num2))
-                    else:
-                    print("{}{}, ".format(num1, num2), end='')
+        else:
+            print("{}{}, ".format(num1, num2), end='')
