5f1764553c7dbaade07ba547585dd9fc816aab34
msDee060 <dananamercy@gmail.com>
2024-03-18 01:02:21 -0700

    Reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for j in range (ord('z'), ord('a') - 1, -1):
+for j in range(ord('z'), ord('a') - 1, -1):
     if j % 2 != 0:
         print("{}".format(chr(j - 32)), end="")
     else:
